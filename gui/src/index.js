0d0199b2f01dfb1a1e08da54f7691a6b795091a5
Ashley Cutmore <acutmore@users.noreply.github.com>
2018-03-02 16:42:15 +0000

    feat(gui): basic interactive GUI where output reflects input

--- gui/src/index.js
+++ gui/src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.onkeyup = () => {
+    output.innerText = input.value;
+};
