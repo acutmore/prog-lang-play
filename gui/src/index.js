80d0d8f1d5aee1c3edaa995d44a604b58e30f860
Ashley Cutmore <acutmore@users.noreply.github.com>
2018-03-02 17:01:26 +0000

    feat(gui): Interactive 'lc' to javascript transpiler

--- gui/src/index.js
+++ gui/src/index.js
@@ -1,6 +1,16 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const compiler = require('../../lc').compileToJs;
 
 input.onkeyup = () => {
-    output.innerText = input.value;
+    try {
+        output.innerText = compiler(input.value);
+    } catch (e) {
+        if (e && e.userMessage) {
+            output.innerText = e.userMessage(input.value);
+        } else {
+            console.error(e);
+            output.innerText = `! Unexpected error`;
+        }
+    }
 };
