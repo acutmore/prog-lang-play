14f370e1480bf9e5189d5891ba5de8a5e907b5fe
Ashley Claymore <acutmore@users.noreply.github.com>
2018-09-06 20:55:54 +0100

    chore(build): build wasm script now working directory safe

--- lc-rust/build_wasm.sh
+++ lc-rust/build_wasm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# change working directory to script location
+cd -P -- "$(dirname -- "$0")"
+
+# compile rust to wasm
 cargo +nightly build --target wasm32-unknown-unknown --release
 
+# copy wasm for use by gui
 cp ./target/wasm32-unknown-unknown/release/lc.wasm ../gui/dist
